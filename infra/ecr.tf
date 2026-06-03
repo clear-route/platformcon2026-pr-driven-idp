e515089b514a749db3c87f89a4c499bedacdcb02
Tom Morelly <tom.morelly@clearroute.io>
2026-06-03 22:10:00 +1000

    undo all

--- infra/ecr.tf
+++ infra/ecr.tf
@@ -3,7 +3,7 @@ resource "aws_ecr_repository" "this" {
 
   name                 = each.key
   image_tag_mutability = "IMMUTABLE"
-  force_delete = true
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
