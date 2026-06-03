e5fe7359c7381f4c935863011d6145c016d221eb
Tom Morelly <tom.morelly@clearroute.io>
2026-06-03 23:12:53 +1000

    demo-app frontend change

--- infra/ecr.tf
+++ infra/ecr.tf
@@ -2,7 +2,7 @@ resource "aws_ecr_repository" "this" {
   for_each = toset([for k in local.matrix : "${k.app}-${k.component}-${k.env}"])
 
   name                 = each.key
-  image_tag_mutability = "IMMUTABLE"
+  image_tag_mutability = "MUTABLE"
   force_delete         = true
 
   image_scanning_configuration {
